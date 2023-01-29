dcb057ffceb9877f0896a3bcb3524410b8170815
alexeyvershinin <og2001@yandex.ru>
2023-01-29 17:18:33 +0300

    Добавил функцию получения url, полученные данные сохранил в item_urls.txt

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ from selenium.webdriver.common.by import By
 import time
 
 
-# получаем исходный код страницы
 def get_sourse_html(url):
+    """
+    Функция получает исходный код страницы
+    :param url: str
+    :return:
+    """
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
 
@@ -32,7 +36,7 @@ def get_sourse_html(url):
 
                 break
 
-            # скролим страницу
+            # скроллим страницу
             else:
                 actions = ActionChains(driver)
                 actions.click(find_more_element).perform()
@@ -45,11 +49,38 @@ def get_sourse_html(url):
         driver.close()
         driver.quit()
 
+def get_items_urls(file_path):
+    """
+    Функция получает ссылки со всех карточек и сохраняет в файл
+    :param file_path: str
+    :return:
+    """
+    with open(file_path, 'r', encoding='utf-8') as file:
+        src = file.read()
+
+    # получаем все карточки со страницы
+    soup = BeautifulSoup(src, 'lxml')
+    items_div = soup.find_all('div', class_='minicard-item__info')
+
+    urls = []
+
+    # получаем ссылки
+    for item in items_div:
+        item_url = item.find('h2', class_='minicard-item__title').find('a').get('href')
+        urls.append(item_url)
+
+    # записываем  полученные url в файл
+    with open('item_urls.txt', 'w', encoding='utf-8') as file:
+        for url in urls:
+            file.write(f'{url}\n')
+
+    return print('[INFO] urls collected successfully')
+
 
 def main():
-    get_sourse_html(
-        url='https://spb.zoon.ru/medical/?search_query_form=1&m%5B5200e522a0f302f066000055%5D=1&center%5B%5D=59.91878264665887&center%5B%5D=30.342586983263384&zoom=10')
-
+    # get_sourse_html(
+    #     url='https://spb.zoon.ru/medical/?search_query_form=1&m%5B5200e522a0f302f066000055%5D=1&center%5B%5D=59.91878264665887&center%5B%5D=30.342586983263384&zoom=10')
+    get_items_urls(file_path='source-page.html')
 
 if __name__ == '__main__':
     main()
